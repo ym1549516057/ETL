d3a5c48654a2c563f9d4726faec3620b71f71582
yangming <1549516057@qq.com>
2020-01-09 11:03:03 +0800

    test

---
+++ src/main/java/com/example/demo/rowmapper/BaseRowMapper.java
@@ -0,0 +1,160 @@
+package com.shufeng.dg.dispatch.rowmapper;
+
+import com.shufeng.dg.dispatch.Annotation.ClobAnna;
+import com.shufeng.dg.dispatch.Annotation.OnlyDate;
+import com.shufeng.dg.dispatch.enums.DatabaseType;
+import com.shufeng.dg.dispatch.utils.ClobUtil;
+import com.shufeng.dg.dispatch.utils.ThreadLoaclUtil;
+import com.shufeng.dg.dispatch.utils.Tool;
+import com.springcloud.framework.utils.DateUtil;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * @author YM
+ * @date 2019/11/25 14:49
+ */
+public class BaseRowMapper<T> implements RowMapper<T> {
+    private Class<?> targetClazz;
+
+    private HashMap<String, Field> fieldMap;
+
+
+    /**
+     * 构造函数.
+     *
+     * @param targetClazz .
+     */
+    public BaseRowMapper(Class<?> targetClazz) {
+        this.targetClazz = targetClazz;
+        fieldMap = new HashMap<>();
+        Field[] fields = targetClazz.getDeclaredFields();
+        for (Field field : fields) {
+            // 同时存入大小写，如果表中列名区分大小写且有列ID和列iD，则会出现异常。
+            // 阿里开发公约，建议表名、字段名必须使用小写字母或数字；禁止出现数字开头，禁止两个下划线中间只出现数字。
+            fieldMap.put(field.getName(), field);
+            // fieldMap.put(getFieldNameUpper(field.getName()), field);
+        }
+    }
+
+    @Override
+    public T mapRow(ResultSet rs, int rowNum) throws SQLException {
+        T obj = null;
+        String columnName = null;
+        String fieldName = null;
+
+        try {
+            obj = (T) targetClazz.newInstance();
+
+            ResultSetMetaData metaData = rs.getMetaData();
+
+            int columnLength = metaData.getColumnCount();
+
+            for (int i = 1; i <= columnLength; i++) {
+                columnName = metaData.getColumnLabel(i);
+                fieldName = Tool.lineToHump(columnName.toLowerCase());
+                Class fieldClazz = fieldMap.get(fieldName).getType();
+                Field field = fieldMap.get(fieldName);
+                field.setAccessible(true);
+                if (ThreadLoaclUtil.databaseTypeTheardLocal.get().equals(DatabaseType.mysql.label())) {
+                    mapToMysql(field, fieldClazz, obj, rs, columnName);
+                }
+                if (ThreadLoaclUtil.databaseTypeTheardLocal.get().equals(DatabaseType.oracle.label())) {
+                    mapToOracle(field, fieldClazz, obj, rs, columnName);
+                }
+                field.setAccessible(false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(columnName);
+            System.out.println(fieldName);
+        }
+        return obj;
+    }
+
+
+    private void mapToMysql(Field field, Class fieldClazz, Object obj, ResultSet rs, String columnName) throws SQLException, IOException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        if (fieldClazz == int.class || fieldClazz == Integer.class) {
+            field.set(obj, rs.getInt(columnName));
+        } else if (fieldClazz == boolean.class || fieldClazz == Boolean.class) {
+            field.set(obj, rs.getBoolean(columnName));
+        } else if (fieldClazz == String.class) {
+            field.set(obj, rs.getString(columnName));
+        } else if (fieldClazz == float.class) {
+            field.set(obj, rs.getFloat(columnName));
+        } else if (fieldClazz == double.class || fieldClazz == Double.class) {
+            field.set(obj, rs.getDouble(columnName));
+        } else if (fieldClazz == BigDecimal.class) {
+            field.set(obj, rs.getBigDecimal(columnName));
+        } else if (fieldClazz == short.class || fieldClazz == Short.class) {
+            field.set(obj, rs.getShort(columnName));
+        } else if (fieldClazz == Date.class) {
+            field.set(obj, new Date(rs.getTimestamp(columnName).getTime()));
+        } else if (fieldClazz == Timestamp.class) {
+            field.set(obj, new Date(rs.getTimestamp(columnName).getTime()));
+        } else if (fieldClazz == Long.class || fieldClazz == long.class) {
+            field.set(obj, rs.getLong(columnName));
+        } else if (fieldClazz.isEnum()) {
+            enumField(field, fieldClazz, obj, rs, columnName);
+        }
+    }
+
+
+    private void mapToOracle(Field field, Class fieldClazz, Object obj, ResultSet rs, String columnName) throws SQLException, IOException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        if (field.isAnnotationPresent(ClobAnna.class)) {
+            field.set(obj, ClobUtil.ClobToString(rs.getClob(columnName)));
+        } else {
+            if (fieldClazz == Integer.class) {
+                field.set(obj, Integer.parseInt(rs.getBigDecimal(columnName) == null ? "0" : rs.getBigDecimal(columnName) + ""));
+            } else if (fieldClazz == Long.class) {
+                field.set(obj, Long.parseLong(rs.getBigDecimal(columnName) == null ? "0" : rs.getBigDecimal(columnName) + ""));
+            } else if (fieldClazz == Double.class) {
+                field.set(obj, Double.parseDouble(rs.getBigDecimal(columnName) == null ? "0" : rs.getBigDecimal(columnName) + ""));
+            } else if (fieldClazz == Float.class) {
+                field.set(obj, Float.parseFloat(rs.getBigDecimal(columnName) == null ? "0" : rs.getBigDecimal(columnName) + ""));
+            } else if (fieldClazz == Date.class) {
+                if (field.isAnnotationPresent(OnlyDate.class)) {
+                    field.set(obj, DateUtil.stringToDate(rs.getString(columnName)));
+                } else {
+                    field.set(obj, DateUtil.stringToDatetime(rs.getString(columnName)));
+                }
+            } else if (fieldClazz == Timestamp.class) {
+                if (field.isAnnotationPresent(OnlyDate.class)) {
+                    field.set(obj, DateUtil.stringToDate(rs.getString(columnName)));
+                } else {
+                    field.set(obj, DateUtil.stringToDatetime(rs.getString(columnName)));
+                }
+            } else if (fieldClazz == Clob.class) {
+                field.set(obj, rs.getClob(columnName).toString());
+            } else if (fieldClazz.isEnum()) {
+                enumField(field, fieldClazz, obj, rs, columnName);
+            } else {
+                field.set(obj, rs.getObject(columnName));
+            }
+
+        }
+    }
+
+    private void enumField(Field field, Class fieldClazz, Object obj, ResultSet rs, String columnName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, SQLException {
+
+        Enum[] enumConstant = (Enum[]) fieldClazz.getEnumConstants();
+        //根据方法名获取方法
+        Method origin = fieldClazz.getMethod("origin");
+        for (Enum en : enumConstant) {
+            //执行方法
+            Integer a = (Integer) origin.invoke(en);
+            if (a == rs.getInt(columnName)) {
+                field.set(obj, en);
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/utils/Tool.java
@@ -0,0 +1,41 @@
+package com.shufeng.dg.dispatch.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author YM
+ * @date 2019/4/25 15:51
+ */
+
+public class Tool {
+    private static Pattern linePattern = Pattern.compile("_(\\w)");
+    /**下划线转驼峰*/
+    public static String lineToHump(String str){
+        str = str.toLowerCase();
+        Matcher matcher = linePattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while(matcher.find()){
+            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+    /**驼峰转下划线(简单写法，效率低于{@link #humpToLine2(String)})*/
+    public static String humpToLine(String str){
+        return str.replaceAll("[A-Z]", "_$0").toLowerCase();
+    }
+    private static Pattern humpPattern = Pattern.compile("[A-Z]");
+    /**驼峰转下划线,效率比上面高*/
+    private static String humpToLine2(String str){
+        Matcher matcher = humpPattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while(matcher.find()){
+            matcher.appendReplacement(sb, "_"+matcher.group(0).toLowerCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+
+}
+
